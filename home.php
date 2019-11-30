2e898378b0bf3ddbd0d76fbe854bf6d4db8bc56c
Gerald Andersen Durano <45748966+GeraldDurano@users.noreply.github.com>
2019-12-01 02:41:27 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,87 @@
+﻿<html>
+<head>
+	<title>Add Data</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	
+<style type="text/css">
+body {
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #585252;
+    background-color: #bdbdbd24;
+}
+	a {
+    color: #00549c;
+    text-decoration: none;
+    font-size: 30px;
+    text-align: center;
+    padding-left: 800px;
+    font-family: 'Sigmar One', cursive;
+}
+
+	h1 {
+    color: #009814;
+    text-decoration: none;
+    font-size: 30px;
+    text-align: center;
+    padding-left: 0.1px;
+    font-family: 'Sigmar One', cursive;
+}
+.navbar-inverse .navbar-nav>li>a {
+    color: #f10000;
+    font-family: initial;
+    font-size: x-large;
+}
+
+</style>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse">
+  <ul class="nav navbar-nav">
+  <li><a href="home.php">Home</a></li>
+    <li><a href="add.html">Student Table</a></li>
+     <li><a href="add2.php">Class Table</a></li>
+       <li><a href="index3.php"> View Class Table</a></li>
+     <li><a href="index2.php"> View Student Table</a></li>
+
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+     
+      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
+    </ul>
+</nav>
+
+<div class="row">
+  <div class="col-sm-4"><h1>Vision</h1>
+  <h4>
+Asian Institute of Computer Studies is a learning institution that upholds the importance of moral values, social responsibility, and Christian formation. The school aims to become an instrument of learning, growth and development to their students, to share knowledge and enhance skills necessary in the completion of their education, and instill the importance of good values and religion to help in their moral and spiritual development.
+
+Thus, Asian Institute of Computer Studies envisions its graduates to be well-rounded individuals who are:
+
+1. Physically fit, emotionally stable, morally sound and intellectually prepared.
+
+2. Responsible individual with high integrity.
+
+3. Service-oriented persons who are responsive to the moral, spiritual, cultural, social and economic needs of their country.
+
+4. Innovative and active participants in the shaping of the future I.T. generation.</h4></div>
+  <div class="col-sm-4">              <h1> Mission </h1>
+  <h4>
+Asian Institute of Computer Studies takes upon itself the mission of providing high-quality yet affordable I.T. education to the Filipino youth, and producing well-rounded graduates who are equipped with the knowledge, skills, and values needed to cope and adapt to the complexities and advancements in the world of Information Technology.
+
+1. Provide an students an affordable but high quality students.
+
+2. Balance their technological education with value-oriented studies.
+
+3. Support the students’ development by providing them responsible and professionally competent faculty, high-class facilities, seminars that keep them abreast of the latest technological breakthroughs, and sufficient practical training through simulated office experience, as well as linkages with reputable business establishments.</h4></div>
+  <div class="col-sm-4" style="text-align:center"><h2>AICS Events and Announcement</h2>
+  <img src="pic5.jpg" hieght="400px" width="400px"></div>
+</div>
+</body>
+</html>
